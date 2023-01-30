26a76357c502e83b01d0196b857555b4b4119275
cypark0531 <cyelp345789@naver.com>
2023-01-31 01:21:58 +0900

    commit test

--- src/main/frontend/src/home.js
+++ src/main/frontend/src/home.js
@@ -15,7 +15,7 @@ const Home = () => {
         <div className="section">
             <h1 className="title algolia-lvl0">WelCome to My Zone</h1>
             <div className="subtitle is-4 algolia-lvl1">
-                <p>이곳은 홈이에요. 가장 먼저 보여지는 페이지죠</p>
+                <p>PCY♥KYR</p>
             </div>
             {message}
             <a href="/about">About</a>
